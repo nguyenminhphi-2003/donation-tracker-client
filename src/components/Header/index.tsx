d68bf9347059c50f095874463e760b7f5f483eec
nguyenminhphi-2003 <nmphi2710@gmail.com>
2025-02-08 20:14:46 +0700

    style: change header position to fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo.png';
 
 function Header() {
     return (
-        <header className='flex justify-between w-full h-13 shadow-md px-2 mb-2 items-center fixed z-100'>
+        <header className='flex fixed justify-between w-full h-13 shadow-md px-2 items-center z-100 bg-white rounded-b-sm'>
             <a href='' className='size-13'>
                 <img src={logo} alt='logo' />
             </a>
