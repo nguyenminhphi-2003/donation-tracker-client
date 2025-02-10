c12524b17dc7b52af8fc0f8351a7365e76836585
nguyenminhphi-2003 <nmphi2710@gmail.com>
2025-02-10 10:10:59 +0700

    feat(app): add outlet component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import Homepage from './pages/Homepage'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  return <Homepage />
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer/>
+    </div>
+  )
 }
 
 export default App
