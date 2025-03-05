80e69cfc71fee7ea09cc0bc29ca5d628b2185f5a
nguyenminhphi-2003 <nmphi2710@gmail.com>
2025-03-05 15:40:04 +0700

    feat(axios): create auth api service

---
+++ src/api/auth.api.ts
@@ -0,0 +1,12 @@
+import api from './axios';
+
+export const authApi = {
+    login: (email: string, password: string) =>
+        api.post(
+            '/users/login',
+            { email, password },
+            { withCredentials: true },
+        ),
+
+    logout: () => api.get('/users/logout'),
+};
