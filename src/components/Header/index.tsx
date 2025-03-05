c2848bf7fc5c196895c4bedf410e2ee38be97dd2
nguyenminhphi-2003 <nmphi2710@gmail.com>
2025-03-05 15:36:54 +0700

    feat(router): add link component routing to homepage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,13 @@
 import HeaderPanel from '../HeaderPanel';
 import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <header className='flex fixed justify-between w-full h-13 shadow-md px-2 items-center z-100 bg-white rounded-b-sm'>
-            <a href='' className='size-13'>
+        <header className='flex fixed top-0 justify-between w-full h-13 shadow-md px-2 items-center z-100 bg-white rounded-b-sm'>
+            <Link to='/' className='size-13'>
                 <img src={logo} alt='logo' />
-            </a>
+            </Link>
             <HeaderPanel />
         </header>
     );
