1c94c616effe4c68193190f0cb3d266cf7c28cac
nguyenminhphi-2003 <nmphi2710@gmail.com>
2025-03-15 12:19:02 +0700

    fix: fix content covered by header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import Footer from './components/Footer'
-import Header from './components/Header'
-import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Outlet />
-      <Footer/>
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <div className='mt-13'>
+                <Outlet />
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
